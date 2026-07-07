bfa066c830e4f7489f221a1c02df41089a065660
Vansh Mahalle <vanshmahale055@gmail.com>
2026-07-08 03:44:10 +0530

    feat: introduce responsive landing page with dashboard routing

--- src/App.js
+++ src/App.js
@@ -1,51 +1,23 @@
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-import { useEffect, useState } from "react";
-
-import Landing from "./pages/Landing";
-import Dashboard from "./pages/Dashboard";
+import { useEffect } from "react";
 
 export default function App() {
-  console.log("APP RENDERED");
-
-  const [token, setToken] = useState(localStorage.getItem("nowthink_token"));
 
   useEffect(() => {
-    console.log("USE EFFECT STARTED");
+
+    alert("USE EFFECT");
 
     const params = new URLSearchParams(window.location.search);
-    const urlToken = params.get("token");
 
-    console.log("TOKEN FROM URL =", urlToken);
+    const token = params.get("token");
 
-    if (urlToken) {
-      console.log("Saving token...");
+    alert(token);
 
-      localStorage.setItem("nowthink_token", urlToken);
-
-      console.log(
-        "Saved token =",
-        localStorage.getItem("nowthink_token")
-      );
-
-      setToken(urlToken);
-
-      window.history.replaceState({}, "", "/app");
+    if(token){
+      localStorage.setItem("nowthink_token", token);
+      alert("saved");
     }
+
   }, []);
 
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route
-          path="/"
-          element={token ? <Navigate to="/app" replace /> : <Landing />}
-        />
-
-        <Route
-          path="/app"
-          element={token ? <Dashboard /> : <Navigate to="/" replace />}
-        />
-      </Routes>
-    </BrowserRouter>
-  );
+  return <h1>Hello App</h1>;
 }
